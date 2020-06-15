3f3aa40b818d34fd05f01eda5c927e3975b561ea
Quentin Smith <quentin@mit.edu>
2020-06-15 00:28:32 -0400

    Parse labeling info

--- qml.js
+++ qml.js
@@ -1,3 +1,4 @@
+// -*- js-indent-level: 8; -*-
 const _ = require('lodash');
 
 function parseEsriLineStyle(style) {
@@ -136,6 +137,31 @@ function parseEsriSymbol(symbol) {
 	}
 }
 
+function parseEsriLabelPlacement(placement) {
+	// placement, placement flags, quad offset
+	return {
+		esriServerPointLabelPlacementAboveCenter: [1, 0, 1],
+		esriServerPointLabelPlacementBelowCenter: [1, 0, 7],
+		esriServerPointLabelPlacementCenterCenter: [1, 0, 4],
+		esriServerPointLabelPlacementAboveLeft: [1, 0, 0],
+		esriServerPointLabelPlacementBelowLeft: [1, 0, 6],
+		esriServerPointLabelPlacementCenterLeft: [1, 0, 3],
+		esriServerPointLabelPlacementAboveRight: [1, 0, 2],
+		esriServerPointLabelPlacementBelowRight: [1, 0, 8],
+		esriServerPointLabelPlacementCenterRight: [1, 0, 5],
+		esriServerLinePlacementAboveAfter: [2, 10, 0],
+		esriServerLinePlacementAboveStart: [2, 10, 0],
+		esriServerLinePlacementAboveAlong: [2, 10, 0],
+		esriServerLinePlacementBelowAfter: [2, 12, 0],
+		esriServerLinePlacementBelowStart: [2, 12, 0],
+		esriServerLinePlacementBelowAlong: [2, 12, 0],
+		esriServerLinePlacementCenterAfter: [2, 9, 0],
+		esriServerLinePlacementCenterStart: [2, 9, 0],
+		esriServerLinePlacementCenterAlong: [2, 9, 0],
+		esriServerPolygonPlacementAlwaysHorizontal: [4, 0, 0],
+	}[placement] || [0, 0, 0];
+}
+
 function toQGISOption(data) {
 	if (typeof data === 'string') {
 		return {
@@ -173,6 +199,15 @@ function toQGISOption(data) {
 			Option: _.map(data, (value, key) => _.merge(toQGISOption(value), {'_attributes': {'name': key}})),
 		};
 	};
+}
+
+function parseEsriLabelingExpression(expression) {
+	return expression
+		.replace(/(?=([^"\\]*(\\.|"([^"\\]*\\.)*[^"\\]*"))*[^"]*$)(\s|^)CONCAT(\s|$)/, '$4||$5')
+		.replace(/(?=([^"\\]*(\\.|"([^"\\]*\\.)*[^"\\]*"))*[^"]*$)(\s|^)NEWLINE(\s|$)/, "$4'\n'$5")
+		.replace(/"(.*?(?<!\\))"/, "'$1'")
+		.replace(/\\"/, '"')
+		.replace(/\[([^\]]*)\]/, '"$1"');
 }
 
 function generateQML(meta) {
@@ -243,79 +278,126 @@ function generateQML(meta) {
 			};
 		}
 	}
+	if (meta.drawingInfo && meta.drawingInfo.labelingInfo) {
+		out.qgis = _.merge(out.qgis, {
+			labeling: {
+				'_attributes': {
+					type: 'rule-based',
+				},
+				rules: {
+					rule: _.map(meta.drawingInfo.labelingInfo, (label) => {
+						let placement = parseEsriLabelPlacement(label.labelPlacement);
+						return {
+							settings: {
+								'text-style': {
+									'_attributes': {
+										fontSizeUnit: 'Point',
+										textColor: label.symbol.color.join(','),
+										fontFamily: label.symbol.font.family,
+										fontSize: label.symbol.font.size,
+										fontWeight: label.symbol.font.weight == 'bold' ? 75 : 50,
+										isExpression: 1,
+										fieldName: parseEsriLabelingExpression(label.labelExpression),
+									},
+								},
+								placement: {
+									'_attributes': {
+										placement: placement[0],
+										placementFlags: placement[1],
+										quadOffset: placement[2],
+									},
+								},
+								rendering: {
+									'_attributes': {
+										scaleMin: label.minScale,
+										scaleMax: label.maxScale,
+									},
+								},
+							},
+						};
+					})
+				},
+			},
+		});
+	}
+
 	if (meta.type == "Annotation Layer") {
-		out.qgis['_attributes'].labelsEnabled = '1';
-		out.qgis['renderer-v2'] = {
+		_.merge(out.qgis, {
 			'_attributes': {
-				type: 'nullSymbol',
+				labelsEnabled: '1',
 			},
-		};
-		out.qgis.labeling = {
-			'_attributes': {
-				type: 'simple',
+			'renderer-v2': {
+				'_attributes': {
+					type: 'nullSymbol',
+				},
 			},
-			settings: {
-				'text-style': {
-					'_attributes': {
-						fieldName: "regexp_replace(TextString, '&lt;[^>]+>','')",
-						isExpression: '1',
-					},
+			labeling: {
+				'_attributes': {
+					type: 'simple',
 				},
-				placement: {
-					'_attributes': {
-						placement: '1',
-					},
-				},
-				dd_properties: {
-					'Option': toQGISOption({
-						name: '',
-						properties: {
-							Bold: {
-								active: true,
-								field: 'Bold',
-								type: 2,
-							},
-							Italic: {
-								active: true,
-								field: 'Italic',
-								type: 2,
-							},
-							Underline: {
-								active: true,
-								field: 'Underline',
-								type: 2,
-							},
-							Color: {
-								active: true,
-								expression: "color_rgb(to_real(coalesce(regexp_substr( TextString, 'red=''([^'']+)'''),0)&#x9;||'.0'),to_real(coalesce(regexp_substr( TextString, 'green=''([^'']+)'''),0)||'.0'),to_real(coalesce(regexp_substr( TextString, 'blue=''([^'']+)'''),0)||'.0'))",
-								type: 3,
-							},
-							Family: {
-								active: true,
-								field: 'FontName',
-								type: 2,
-							},
-							LabelRotation: {
-								active: true,
-								expression: '-"Angle"',
-								type: 3,
-							},
-							OffsetXY: {
-								active: true,
-								expression: 'array(XOffset,YOffset)',
-								type: 3,
-							},
-							Size: {
-								active: true,
-								field: 'FontSize',
-								type: 2,
-							},
+				settings: {
+					'text-style': {
+						'_attributes': {
+							fieldName: "regexp_replace(TextString, '&lt;[^>]+>','')",
+							isExpression: '1',
 						},
-						type: 'collection',
-					}),
+					},
+					placement: {
+						'_attributes': {
+							placement: '1',
+						},
+					},
+					dd_properties: {
+						'Option': toQGISOption({
+							name: '',
+							properties: {
+								Bold: {
+									active: true,
+									field: 'Bold',
+									type: 2,
+								},
+								Italic: {
+									active: true,
+									field: 'Italic',
+									type: 2,
+								},
+								Underline: {
+									active: true,
+									field: 'Underline',
+									type: 2,
+								},
+								Color: {
+									active: true,
+									expression: "color_rgb(to_real(coalesce(regexp_substr( TextString, 'red=''([^'']+)'''),0)&#x9;||'.0'),to_real(coalesce(regexp_substr( TextString, 'green=''([^'']+)'''),0)||'.0'),to_real(coalesce(regexp_substr( TextString, 'blue=''([^'']+)'''),0)||'.0'))",
+									type: 3,
+								},
+								Family: {
+									active: true,
+									field: 'FontName',
+									type: 2,
+								},
+								LabelRotation: {
+									active: true,
+									expression: '-"Angle"',
+									type: 3,
+								},
+								OffsetXY: {
+									active: true,
+									expression: 'array(XOffset,YOffset)',
+									type: 3,
+								},
+								Size: {
+									active: true,
+									field: 'FontSize',
+									type: 2,
+								},
+							},
+							type: 'collection',
+						}),
+					},
 				},
 			},
-		};
+		});
 	}
 
 	out.qgis.fieldConfiguration.field = meta.fields.map(function(field) {
